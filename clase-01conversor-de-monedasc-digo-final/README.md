f00d69e8861fe2030e90ad376643717a54d46248
jesicaAmaya <jesicaeamaya@gmail.com>
2024-03-31 23:46:58 -0300

    first commit

---
+++ clase-01conversor-de-monedasc-digo-final/README.md
@@ -0,0 +1,4 @@
+# Clase 01 - Conversor de monedas - Código Final
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jesicaeamaya/pen/gOyGLYE](https://codepen.io/jesicaeamaya/pen/gOyGLYE).
+
---
+++ clase-01conversor-de-monedasc-digo-final/src/script.js
@@ -0,0 +1,8 @@
+var valorEnDolar = 60;
+var cotizacionDelDolar = 16.89;
+
+var valorEnPesosMexicanos = valorEnDolar * cotizacionDelDolar
+
+valorEnPesosMexicanos = valorEnPesosMexicanos.toFixed(2)
+
+alert("$" + valorEnPesosMexicanos)
